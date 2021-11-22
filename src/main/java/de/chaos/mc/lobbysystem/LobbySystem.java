16a2a4cd2997c63ca30814a038f62a32f05c00c3
Chaoshero5567 <79978761+Chaoshero5567@users.noreply.github.com>
2021-11-22 17:02:40 +0100

    Inital ProjectSetup

---
+++ src/main/java/de/chaos/mc/lobbysystem/LobbySystem.java
@@ -0,0 +1,17 @@
+package de.chaos.mc.lobbysystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LobbySystem extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onLoad() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
