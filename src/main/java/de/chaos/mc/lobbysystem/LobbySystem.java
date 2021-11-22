e5bfd310c2a889452892e3742822b70565d43e38
UnlegitDqrk <myserver312@gmail.com>
2021-11-22 21:13:24 +0100

    Added Server API

--- src/main/java/de/chaos/mc/lobbysystem/LobbySystem.java
+++ src/main/java/de/chaos/mc/lobbysystem/LobbySystem.java
@@ -3,15 +3,14 @@ package de.chaos.mc.lobbysystem;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LobbySystem extends JavaPlugin {
-    @Override
-    public void onEnable() {
-    }
 
     @Override
-    public void onLoad() {
+    public void onEnable() {
+        super.onEnable();
     }
 
     @Override
     public void onDisable() {
+        super.onDisable();
     }
 }
